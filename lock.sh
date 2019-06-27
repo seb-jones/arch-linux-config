e74562d9e79c91a5dd1b5a4987741029f828e7ae
Seb Jones <seb.jones@enovate.co.uk>
2019-06-27 08:50:01 +0100

    Improve file handling in lock script.

--- lock.sh
+++ lock.sh
@@ -2,11 +2,12 @@
 
 if [ -e '/home/seb/.fehbg' ]
 then
-    IMAGE=`grep -o \/.*\.png /home/seb/.fehbg`
+    IMAGE=`grep -o "'[^']*\.png'" /home/seb/.fehbg | sed -E -e "s/(^'|'$)//g"`
+    IMAGE="/home/seb/$IMAGE"
 
     if [ $? -eq 0 ]
     then
-        i3lock -i $IMAGE
+        i3lock -tiling -i $IMAGE
         exit 0
     fi
 fi
