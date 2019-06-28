78aae67f439ae7707d7eaf18073426b5d7bcfb5f
Seb Jones <jones.seb95@googlemail.com>
2019-06-28 19:28:14 +0100

    Make lock script work with both relative and absolute paths.

--- lock.sh
+++ lock.sh
@@ -2,14 +2,20 @@
 
 if [ -e '/home/seb/.fehbg' ]
 then
-    IMAGE=`grep -o "'[^']*\.png'" /home/seb/.fehbg | sed -E -e "s/(^'|'$)//g"`
-    IMAGE="/home/seb/$IMAGE"
 
+    IMAGE=`grep -o "'[^']*\.png'" /home/seb/.fehbg | sed -E -e "s/(^'|'$)//g"`
+
+    # Make image path absolute if it is relative
+    echo "$IMAGE" | grep -E '(^/|^~)' || IMAGE="/home/seb/$IMAGE"
+
+    # If $IMAGE is a valid file, lock with it
+    ls "$IMAGE" 2> /dev/null
     if [ $? -eq 0 ]
     then
         i3lock -tiling -i $IMAGE
         exit 0
     fi
+
 fi
         
 i3lock
