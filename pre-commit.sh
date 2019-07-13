9ef23f3011aaa44ac9630f292ead50bc06322543
Seb Jones <jones.seb95@googlemail.com>
2019-07-13 09:37:59 +0100

    Generate i3status config example file in pre-commit.

--- pre-commit.sh
+++ pre-commit.sh
@@ -2,5 +2,6 @@
 
 cp ~/.config/Xresources ~/.config/Xresources.example
 cp ~/.config/i3/config ~/.config/i3/config.example
+cp ~/.config/i3status/config ~/.config/i3status/config.example
 
 git add .
