086b0581734163a66741678567f0f42c49271cbd
Seb Jones <jones.seb95@googlemail.com>
2019-10-26 17:50:36 +0100

    Pause dunst notifications before i3lock and resume after.

--- lock.sh
+++ lock.sh
@@ -6,39 +6,28 @@ BLUE='#008fd6bb'
 WHITE='#ffffffbb'
 RED='#bb0000bb'
 
+notify-send "DUNST_COMMAND_PAUSE"
+
 i3lock \
--i ~/.config/screen-lock-overlay.png \
--t \
---insidecolor=$DARK_GREY      \
---ringcolor=$BLUE        \
---linecolor=$LIGHT_GREY        \
---separatorcolor=$BLUE   \
-\
---insidevercolor=$DARK_GREY   \
---ringvercolor=$WHITE     \
-\
---insidewrongcolor=$DARK_GREY \
---ringwrongcolor=$RED   \
-\
---verifcolor=$WHITE        \
---wrongcolor=$WHITE        \
---keyhlcolor=$WHITE       \
---timecolor=$WHITE        \
---datecolor=$WHITE        \
---bshlcolor=$DARK_GREY        \
---clock \
---indicator           \
---blur 6             
-# --layoutcolor=$DARK_GREY      \
-# --keylayout 2         
-# --screen 1            \
-# --timestr="%H:%M:%S"  \
-# --datestr="%A, %m %Y" \
-# --clock               \
-# --veriftext="Drinking verification can..."
-# --wrongtext="Nope!"
-# --textsize=20
-# --modsize=10
-# --timefont=comic-sans
-# --datefont=monofur
-# etc
+    -n \
+    -i ~/.config/screen-lock-overlay.png \
+    -t \
+    --insidecolor=$DARK_GREY      \
+    --ringcolor=$BLUE        \
+    --linecolor=$LIGHT_GREY        \
+    --separatorcolor=$BLUE   \
+    --insidevercolor=$DARK_GREY   \
+    --ringvercolor=$WHITE     \
+    --insidewrongcolor=$DARK_GREY \
+    --ringwrongcolor=$RED   \
+    --verifcolor=$WHITE        \
+    --wrongcolor=$WHITE        \
+    --keyhlcolor=$WHITE       \
+    --timecolor=$WHITE        \
+    --datecolor=$WHITE        \
+    --bshlcolor=$DARK_GREY        \
+    --clock \
+    --indicator           \
+    --blur 6             
+
+notify-send "DUNST_COMMAND_RESUME"
