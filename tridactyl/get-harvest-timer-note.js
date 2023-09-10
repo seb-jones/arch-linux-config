fe1e2a56671fea1c7fe8c95f6515339dedffcf06
Seb Jones <seb.jones@enovate.co.uk>
2023-09-10 12:10:09 +0100

    Add rollbar error page to tridactyl harvest timer yank script

--- tridactyl/get-harvest-timer-note.js
+++ tridactyl/get-harvest-timer-note.js
@@ -3,3 +3,7 @@ const location = window.location.toString();
 if (location.startsWith('https://projects.enovate.co.uk/desk/')) {
     `Ticket ${document.querySelector('.ticket-id').innerText}: ${document.querySelector('.title__subject-text').innerText}`
 }
+
+if (location.startsWith('https://app.rollbar.com/')) {
+    `Rollbar Error ${document.querySelector('head title').innerText}`
+}
